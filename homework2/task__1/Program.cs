757ea2c572c143d19ad5fa06cce3dafabb6e055f
bakasura980 <bakasura@abv.bg>
2015-07-22 19:02:29 +0300

    add

---
+++ homework2/task__1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte value1 = -44, value11 = -115, value12 = 112;
+            byte value2 = 97, value10 = 224;
+            short value3 = -10000, value13 = 1990;
+            ushort value4 = 20000;
+            int value5 = -1000000, value9 = 52130;
+            uint value6 = 4825932;
+            long value7 = 970700000;
+            ulong value8 = 123456789123456789;
+        }
+    }
+}
